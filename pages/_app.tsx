d5d513ebc6692245229e4ebcbcd96e42b00c9fe2
Joshua Nataniel Manuputty <clsjoshua@gmail.com>
2024-06-09 15:40:57 +0700

    feat: convert cosmos-kit to graz

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,58 +1,41 @@
 import "../styles/globals.css";
-import "@interchain-ui/react/styles";
 
 import type { AppProps } from "next/app";
-import { SignerOptions, wallets } from "cosmos-kit";
-import { ChainProvider } from "@cosmos-kit/react";
-import { assets, chains } from "chain-registry";
-import {
-  Box,
-  ThemeProvider,
-  useColorModeValue,
-  useTheme,
-} from "@interchain-ui/react";
+import { ConfigureGrazArgs, GrazProvider } from "graz";
+import { cosmoshub } from "graz/chains";
 
 function CreateCosmosApp({ Component, pageProps }: AppProps) {
-  const { themeClass } = useTheme();
+  const chains = [
+    {
+      ...cosmoshub,
+      rpc: "https://rpc.cosmos.directory/cosmoshub",
+      rest: "https://rest.cosmos.directory/cosmoshub",
+    },
+  ];
 
-  const signerOptions: SignerOptions = {
-    // signingStargate: () => {
-    //   return getSigningCosmosClientOptions();
-    // }
+  const grazOptions: ConfigureGrazArgs = {
+    chains,
+    onReconnectFailed: () => {
+      console.log("reconnect failed");
+    },
+    walletConnect: {
+      options: {
+        projectId: "a8510432ebb71e6948cfd6cde54b70f7",
+        relayUrl: "wss://relay.walletconnect.org",
+        metadata: {
+          name: "CosmosKit to Graz Template",
+          description: "CosmosKit to Graz dapp template",
+          url: "https://docs.cosmology.zone/cosmos-kit/",
+          icons: [],
+        },
+      },
+    },
   };
 
   return (
-    <ThemeProvider>
-      <ChainProvider
-        chains={chains}
-        assetLists={assets}
-        wallets={wallets}
-        walletConnectOptions={{
-          signClient: {
-            projectId: "a8510432ebb71e6948cfd6cde54b70f7",
-            relayUrl: "wss://relay.walletconnect.org",
-            metadata: {
-              name: "CosmosKit Template",
-              description: "CosmosKit dapp template",
-              url: "https://docs.cosmology.zone/cosmos-kit/",
-              icons: [],
-            },
-          },
-        }}
-        // @ts-ignore
-        signerOptions={signerOptions}
-      >
-        <Box
-          className={themeClass}
-          minHeight="100dvh"
-          backgroundColor={useColorModeValue("$white", "$background")}
-        >
-          {/* TODO fix type error */}
-          {/* @ts-ignore */}
-          <Component {...pageProps} />
-        </Box>
-      </ChainProvider>
-    </ThemeProvider>
+    <GrazProvider grazOptions={grazOptions}>
+      <Component {...pageProps} />
+    </GrazProvider>
   );
 }
 
